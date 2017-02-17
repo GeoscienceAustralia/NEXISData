91a8162e89577b4d505369151b83782b53422105
Brad Cook <brad.cook@ga.gov.au>
2017-02-17 16:40:13 +1100

    Fix s3 path in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-aws s3 sync app s3://nexis.et.web.com --region=ap-southeast-2 --delete
-aws s3 sync node_modules s3://nexis.et.web.com --region=ap-southeast-2 --delete
+aws s3 sync app s3://nexis.et.web --region=ap-southeast-2 --delete
+aws s3 sync node_modules s3://nexis.et.web --region=ap-southeast-2 --delete
