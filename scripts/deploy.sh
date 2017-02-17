c7c232a1b1d70a7283860f3d59d037272178a7ec
Brad Cook <brad.cook@ga.gov.au>
2017-02-17 16:22:38 +1100

    Changed deploy script to shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 aws s3 sync app s3://nexis.et.web.com --region=ap-southeast-2 --delete
 aws s3 sync node_modules s3://nexis.et.web.com --region=ap-southeast-2 --delete
