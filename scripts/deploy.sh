ad53e3117c8e1a290dc41ae1ab2055e2bc1685a2
Brad Cook <brad.cook@ga.gov.au>
2017-02-20 16:37:33 +1100

    Attempt to fix s3 sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 # Use the sync tool to update all the files in the app and node_modules
 # folders.
 echo "Syncing app folders to s3 bucket."
-aws s3 sync app s3://nexis.et.web --region=ap-southeast-2 --delete
-aws s3 sync node_modules s3://nexis.et.web --region=ap-southeast-2 --delete
+~/.local/bin aws s3 sync app s3://nexis.et.web --region=ap-southeast-2 --delete
+~/.local/bin aws s3 sync node_modules s3://nexis.et.web --region=ap-southeast-2 --delete
