4da3f5e82b55cda998b1e641c5cf31b5165f881f
Brad Cook <brad.cook@ga.gov.au>
2017-02-20 14:24:16 +1100

    Add comments to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Use the sync tool to update all the files in the app and node_modules
+# folders.
 aws s3 sync app s3://nexis.et.web --region=ap-southeast-2 --delete
 aws s3 sync node_modules s3://nexis.et.web --region=ap-southeast-2 --delete
