7578e4cca382017d7c61ea59fdd1cf1f6067b9e3
Brad Cook <brad.cook@ga.gov.au>
2017-02-22 10:50:07 +1100

    Another attempt at acl deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ echo "Syncing app folders to s3 bucket."
 aws s3 sync app s3://nexis.et.web/app --region=ap-southeast-2 --delete
 aws s3 sync node_modules s3://nexis.et.web/node_modules --region=ap-southeast-2 --delete
 echo "Granting public read permissions to bucket."
-aws s3api put-object-acl --bucket nexis.et.web --key app --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
-aws s3api put-object-acl --bucket nexis.et.web --key node_modules --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3api put-object-acl --bucket nexis.et.web --key app/* --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3api put-object-acl --bucket nexis.et.web --key node_modules/* --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
