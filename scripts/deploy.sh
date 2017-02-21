f211e2b55867c272b459f911fdd67d2643bf41b5
Brad Cook <brad.cook@ga.gov.au>
2017-02-21 11:30:24 +1100

    Fix to bucket permissions on deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ echo "Syncing app folders to s3 bucket."
 aws s3 sync app s3://nexis.et.web/app --region=ap-southeast-2 --delete
 aws s3 sync node_modules s3://nexis.et.web/node_modules --region=ap-southeast-2 --delete
 echo "Granting public read permissions to bucket."
-aws s3api put-bucket-acl --bucket nexis.et.web --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3api put-bucket-acl --bucket nexis.et.web --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
