7f4cbb016b2a905df0dd7cb200a0f33f8909a7fb
Brad Cook <brad.cook@ga.gov.au>
2017-02-27 15:46:45 +1100

    Changed node_modules folder to sit under app folder. - This makes it easier to configure in S3.

--- karma.conf.js
+++ karma.conf.js
@@ -13,8 +13,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'node_modules/angular/angular.js',
-        'node_modules/angular-mocks/angular-mocks.js',
+        'app/node_modules/angular/angular.js',
+        'app/node_modules/angular-mocks/angular-mocks.js',
         'app/**/*.js',
         'app/**/*.html',
         'tests/**/*.js'
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,7 @@
 
 # Use the sync tool to update all the files in the app and node_modules
 # folders.
-echo "Syncing app folders to s3 bucket."
+echo "Syncing app folder to s3 bucket."
 aws s3 sync app s3://nexis.et.web/app --region=ap-southeast-2 --delete
-aws s3 sync node_modules s3://nexis.et.web/node_modules --region=ap-southeast-2 --delete
 echo "Granting public read permissions to bucket."
 aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key app\*
-aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key node_modules\*
