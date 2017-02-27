50a04d0a7ad27023c5a4d9ce8a3c85237b45588b
Brad Cook <brad.cook@ga.gov.au>
2017-02-27 16:47:01 +1100

    Change deploy location

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 # Use the sync tool to update all the files in the app and node_modules
 # folders.
 echo "Syncing app folder to s3 bucket."
-aws s3 sync app s3://nexis.et.web/app --region=ap-southeast-2 --delete
+aws s3 sync app s3://nexis.et.web --region=ap-southeast-2 --delete
 echo "Granting public read permissions to bucket."
-aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key app/
+aws s3api put-object-acl --acl public-read --bucket nexis.et.web
