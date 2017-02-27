4dcfd5f96ab12ee8b68d9dd8909ec03e2bf7126d
Brad Cook <brad.cook@ga.gov.au>
2017-02-27 14:42:14 +1100

    Slight change to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ echo "Syncing app folders to s3 bucket."
 aws s3 sync app s3://nexis.et.web/app --region=ap-southeast-2 --delete
 aws s3 sync node_modules s3://nexis.et.web/node_modules --region=ap-southeast-2 --delete
 echo "Granting public read permissions to bucket."
-aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key app/*
-aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key node_modules/*
+aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key app\*
+aws s3api put-object-acl --acl public-read --bucket nexis.et.web --key node_modules\*
